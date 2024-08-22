0569da53b19295703e7be6d1c1fa84b68aa6cf65
Renato Khael <renatokhael@gmail.com>
2024-08-21 22:19:56 -0300

    feat: create basic components

---
+++ frontend/src/components/Header/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="header">
+    <div class="logo">NETSHOES</div>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/wishlist">Wishlist</router-link>
+    </nav>
+  </header>
+</template>
+
+<script setup lang="ts">
+import "./Header.scss";
+</script>
